9d422bd5cc7d808d1dbbee07e187ae7a57fa1f99
jcedielb <jj.cediel@uniandes.edu.co>
2025-03-21 07:42:07 -0500

    feat: add full-screen image viewer and product image carousel

---
+++ lib/widgets/full_screen_image_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class FullScreenImagePage extends StatelessWidget {
+  final String imageUrl;
+
+  const FullScreenImagePage({Key? key, required this.imageUrl})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        leading: IconButton(
+          icon: const Icon(Icons.close, color: Colors.white),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
+      body: Center(
+        child: InteractiveViewer(
+          child: Image.network(imageUrl, fit: BoxFit.contain),
+        ),
+      ),
+    );
+  }
+}
