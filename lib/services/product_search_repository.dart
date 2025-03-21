3854b15f9eeb2a09154ca7190faa2e7068eeef5e
jcedielb <jj.cediel@uniandes.edu.co>
2025-03-21 06:25:30 -0500

    fix: update Algolia credentials and index name in product search repository

--- lib/services/product_search_repository.dart
+++ lib/services/product_search_repository.dart
@@ -3,12 +3,12 @@ import 'package:algolia/algolia.dart';
 class ProductSearchRepository {
   // Configura tu instancia de Algolia con tus credenciales.
   static final Algolia algolia = Algolia.init(
-    applicationId: 'R93QX2D4XM', // Reemplaza por tu Application ID
-    apiKey: 'cb8807b005fd94e25562c67ffee2b231', // Reemplaza por tu API Key de búsqueda
+    applicationId: 'AAJ6U9G25X', // Reemplaza por tu Application ID
+    apiKey: 'e1450d2b94d56f3a2bf7a7978f255be1', // Reemplaza por tu API Key de búsqueda
   );
 
   Future<List<Map<String, dynamic>>> searchProducts(String query) async {
-    final AlgoliaQuery algoliaQuery = algolia.instance.index('products_senemarket_index').query(query);
+    final AlgoliaQuery algoliaQuery = algolia.instance.index('senemarket_products_index').query(query);
     final AlgoliaQuerySnapshot snapshot = await algoliaQuery.getObjects();
     return snapshot.hits.map((hit) {
       final data = Map<String, dynamic>.from(hit.data);
